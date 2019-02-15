9aa8a64eca5b0d0d92e2b30620977f67a0db0530
dotbyte <dotbyte@163.com>
2019-02-15 14:48:21 +0800

    添加项目文件。

---
+++ ConnectionBase/BaseInterface.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionBase
+{
+    interface BaseInterface
+    {
+        List<string> LogList
+        {
+            get;
+        }
+
+    }
+    public interface ConnectionDriver
+    {
+        string ConnectionString
+        {
+            get;set;
+        }
+        List<string> LogList
+        {
+            get;
+        }
+        Dictionary<int,object> ValueList
+        {
+            get;
+        }
+        int TryConnection();
+    }
+    public interface ConnectionBase
+    {
+        ConnectionDriver Driver { get; set; }
+        int TryConnection();
+        int Connect();
+        int ReadData();
+        int SendToServer();
+        int AcceptRequest();
+        int ResponseData();
+    }
+}
