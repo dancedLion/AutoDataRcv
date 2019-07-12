2f145a747bf086812c8284b354be4bed3c1bccee
yuchen <xuzhen@xinchj.com>
2019-07-12 20:00:13 +0800

    20190712hone

--- WebServiceListener/MeasOfIronAndBasket.cs
+++ WebServiceListener/MeasOfIronAndBasket.cs
@@ -10,30 +10,124 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
 using CHQ.RD.DriverBase;
 using CHQ.RD.DataContract;
+using System.Threading;
+using GeneralOPs;
 namespace CHQ.RD.WebServiceListener
 {
     public class MeasOfIronAndBasket:DriverBase.DriverBase
     {
-        
+        #region 局部变量与属性
+        private string settingsfile = AppDomain.CurrentDomain.BaseDirectory + "IronAndBasket.xml";
+        string errorfile = AppDomain.CurrentDomain.BaseDirectory + "WebServiceListenerError.xml";
+        DateTime currenttime = DateTime.Now;
+        ManualResetEvent mre = new ManualResetEvent(false);
+        Thread m_readthread = null;
+        List<MeasRow> m_items = new List<MeasRow>();
+        Queue<ListKeyValue> m_values = new Queue<ListKeyValue>();
+        #endregion
+
         public MeasOfIronAndBasket() : base()
         {
-
+            m_items.Add(new MeasRow
+            {
+                MaterialType = "Iron",
+                LastQueryTime = getLastDate("Iron")
+            });
+            m_items.Add(new MeasRow
+            {
+                MaterialType = "Basket",
+                LastQueryTime = getLastDate("Basket")
+            });
+            m_readthread = new Thread(ReadingValue);
         }
 
         public override int AcceptSetting(object host, object list)
         {
-            return base.AcceptSetting(host, list);
+            //return base.AcceptSetting(host, list);
+            return 0;
         }
         public override int Start()
         {
-            return base.Start();
+            int ret = 0;
+            m_readthread.Start();
+            mre.Set();
+            return ret;
         }
         public override int Stop()
         {
-            return base.Stop();
+            mre.Reset();
+            return 0;
+            //return base.Stop();
         }
-        
+        /// <summary>
+        /// 根据取回的计量数据转换为字符并传给驱动连接器
+        /// </summary>
+        public void ReadingValue()
+        {
+            try
+            {
+                mre.WaitOne();
+                
+                UASV.ChqUASettingsClient client = new UASV.ChqUASettingsClient();
+                //读取数据
+                //如果有行则写最新的日期
+                //最新的日期是指计量行中的最大的净重日期
+                //currenttime=
+                m_values.Enqueue(new ListKeyValue
+                {
+
+                })
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        public DateTime getLastDate(string materialtype)
+        {
+            DateTime dt = DateTime.Now;
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(settingsfile);
+                dt = DateTime.Parse(doc.DocumentElement.SelectSingleNode("LastTime").Attributes[materialtype].Value);
+            }
+            catch(Exception ex)
+            {
+                TxtLogWriter.WriteErrorMessage(errorfile, this.GetType().FullName + ".getLastDate Error:" + ex.Message);
+            }
+            return dt;
+        }
+        public int saveLastDate(DateTime dt,string materialtype)
+        {
+            int ret = -1;
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(settingsfile);
+                XmlElement node =(XmlElement)doc.DocumentElement.SelectSingleNode("LastTime");
+                if (node != null)
+                {
+                    node.SetAttribute(materialtype, dt.ToString("yyyy-MM-dd hh:mm:ss fff"));
+                }
+                doc.Save(settingsfile);
+                ret = 0;
+            }
+            catch(Exception ex)
+            {
+                TxtLogWriter.WriteErrorMessage(errorfile, this.GetType().FullName + ".saveLastDate Error:" + ex.Message);
+            }
+            return ret;
+        }
+    }
+    public class MeasRow
+    {
+        public string MaterialType;
+        public string MeasureResult;
+        public DateTime LastQueryTime;
     }
 }
